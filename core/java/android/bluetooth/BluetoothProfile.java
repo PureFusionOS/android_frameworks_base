8bb56592bbeb2f617d8f30edbecb6499300fd204
Nitin Shivpure <nshivpur@codeaurora.org>
2015-07-14 12:47:56 +0530

    Bluetooth: DUN: Add framework changes to support DUN
    Bluetooth DUN profile APIs added to the framework APIs.
    As DUN server supports only disconnection of DUN profile
    level connection this API is implemeted along with other
    APIs. It registers bluetooth state changed callback with the
    adapter service,once there is a change in the bluetooth
    state,it will bind/unbind the DUN service which is
    implemented in BluetoothExt.APK.
    
    DUN service, and other DUN Application layer are implemented
    in BluetoothExt.APK.
    
    CRs-Fixed: 557180
    Change-Id: I6ce1204ac6138e28ad7486f2855f0f7444ad3886

--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -137,6 +137,12 @@ public interface BluetoothProfile {
     public static final int PBAP_CLIENT = 17;
 
     /**
+     * DUN
+     * @hide
+     */
+    public static final int DUN = 21;
+
+    /**
      * Max profile ID. This value should be updated whenever a new profile is added to match
      * the largest value assigned to a profile.
      * @hide
