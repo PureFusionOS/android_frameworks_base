8bcb31eb87d599894d0de37b606b548f54b4097b
Alex Cruz <mazdarider23@gmail.com>
2015-12-24 20:34:11 +0000

    [frameworks/base] Show 4G intead of LTE switch
    - Added to statusbar items under the AOSP tuner instead of 2 parts
    - Added a category to organize things a bit
    - Hide the switch on WiFi-Only devices (thanks AICP for the idea)
    - Removed config that was used in overlays
    
    @mydongistiny: use restartUI api
    Change-Id: Id0acf0a6b4bd47f5cefba48ffdeb8e546fd439c9
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4286,6 +4286,12 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * Show four g instead of lte
+         * @hide
+         */
+        public static final String SHOW_FOURG = "show_fourg";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
