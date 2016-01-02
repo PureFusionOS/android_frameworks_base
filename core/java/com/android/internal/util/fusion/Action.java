a76a79e5b4f31f3db22cdf038fe9769cb3882b0c
Jubakuba <Jubakuba@gmail.com>
2016-01-02 22:01:36 +0100

    [frameworks/base] Slim Shortcuts
    commit dae2b22dda55f7159ce3b24a6e14b48db301db2a
    Author: eyosen <abittin@gmail.com>
    Date:   Sat Jan 2 12:59:00 2016 -0800
    
        slimactions: add missing action icon
    
        Change-Id: I5e4df86964192c945b8f6daa54a86abcc7675371
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 89d395f815ff3df9b54313cf7941764a8d10e71b
    Author: LorDClockaN <lordclockan@gmail.com>
    Date:   Thu Jun 16 19:58:47 2016 +0200
    
        SlimShortcuts: Add clear notifications action
    
        Change-Id: Ia1e9c3e4eed604a0425ef39256b6eb2720f1a213
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit d04e88fa27b20d10526d9aebdc50c375528abd25
    Author: Jubakuba <Jubakuba@gmail.com>
    Date:   Wed Sep 2 15:07:19 2015 -0700
    
        Add Screen Sleep shortcut
    
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit d160fd56d5537bc8c50204a201185589df00888a
    Author: LorDClockaN <davor@losinj.com>
    Date:   Wed Sep 2 17:01:22 2015 +0200
    
        Fix up chamber and Torch shortcuts
    
        Change-Id: I33726ff800a145d25bf2190839c4058e67379d21
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit b0a1bfa8eda18175ff733422df9753fe091f09c9
    Author: Jubakuba <Jubakuba@gmail.com>
    Date:   Sun Apr 12 09:40:01 2015 -0600
    
        Frameworks: Chamber of Secrets [1/2]
    
        Add torch shortcut
    
        Change-Id: I2655d304dc30191a92c4c0e921272736c5fff7d6
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    commit 842e96538e956fcf3b9e96184fc6b8493f97281c
    Author: Jubakuba <Jubakuba@gmail.com>
    Date:   Sat Jan 2 22:01:36 2016 +0100
    
        Frameworks: Slim Shortcuts
    
        Change-Id: I784b4fb37238658131779eae3b55adc0aefd8d18
        Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    Change-Id: I10b08d30d674b5d848b797a6672c46a136a28aea
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- core/java/com/android/internal/util/fusion/Action.java
+++ core/java/com/android/internal/util/fusion/Action.java
@@ -51,6 +51,8 @@ import java.net.URISyntaxException;
 
 public class Action {
 
+    private static boolean mTorchEnabled = false;
+
     private static final int MSG_INJECT_KEY_DOWN = 1066;
     private static final int MSG_INJECT_KEY_UP = 1067;
 
@@ -130,6 +132,22 @@ public class Action {
                 context.sendBroadcastAsUser(
                         new Intent("android.settings.SHOW_INPUT_METHOD_PICKER"),
                         new UserHandle(UserHandle.USER_CURRENT));
+                return;
+            } else if (action.equals(ActionConstants.ACTION_TORCH)) {
+                try {
+                    CameraManager cameraManager = (CameraManager)
+                            context.getSystemService(Context.CAMERA_SERVICE);
+                    for (final String cameraId : cameraManager.getCameraIdList()) {
+                        CameraCharacteristics characteristics =
+                            cameraManager.getCameraCharacteristics(cameraId);
+                        int orient = characteristics.get(CameraCharacteristics.LENS_FACING);
+                        if (orient == CameraCharacteristics.LENS_FACING_BACK) {
+                            cameraManager.setTorchMode(cameraId, !mTorchEnabled);
+                            mTorchEnabled = !mTorchEnabled;
+                        }
+                    }
+                } catch (CameraAccessException e) {
+                }
                 return;
             } else if (action.equals(ActionConstants.ACTION_VOICE_SEARCH)) {
                 // launch the search activity
