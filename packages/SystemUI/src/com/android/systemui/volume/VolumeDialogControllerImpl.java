04bc227ff59e1452e0d2a296481116f701aa1a6a
Roman Birg <roman@cyngn.com>
2016-02-05 10:53:12 -0800

    [frameworks/base] Add volume up/down tones back
    During the move from VolumePanel to the new VolumeController/Dialog
    setup, the volume tone logic got removed.
    
    PureNexus edit:
    * Adapt to 8.0's VolumeDialogControllerImpl
    
    DU edit:
    * Get rid of the conditional statement that requires Settings, which makes this enabled by default.
      In our opinion, this enhances UX -- therefore, there is no real requirement to make it optional.
      It can always be revisited at a later date, if necessary.
    
    Change-Id: I75d2d32ed0b48c86c33314456b57e44fbad3a4b4
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -111,6 +111,8 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
     private UserActivityListener mUserActivityListener;
     private ToneGenerator mToneGenerators[];
 
+    private ToneGenerator mToneGenerators[];
+
     public VolumeDialogControllerImpl(Context context) {
         mContext = context.getApplicationContext();
         Events.writeEvent(mContext, Events.EVENT_COLLECTION_STARTED);
@@ -704,6 +706,7 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
+<<<<<<< HEAD
                 case VOLUME_CHANGED:
                     onVolumeChangedW(msg.arg1, msg.arg2);
                     break;
@@ -894,6 +897,60 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
                         entry.getKey().onAccessibilityModeChanged(show);
                     }
                 });
+            }
+        }
+    }
+
+    protected void onPlaySoundW(int streamType, int flags) {
+
+        if (mWorker.hasMessages(W.STOP_SOUNDS)) {
+            mWorker.removeMessages(W.STOP_SOUNDS);
+            // Force stop right now
+            onStopSoundsW();
+        }
+
+        ToneGenerator toneGen = getOrCreateToneGeneratorW(streamType);
+        if (toneGen != null) {
+            toneGen.startTone(ToneGenerator.TONE_PROP_BEEP);
+            mWorker.sendMessageDelayed(mWorker.obtainMessage(W.STOP_SOUNDS), BEEP_DURATION);
+        }
+
+        mWorker.removeMessages(W.FREE_RESOURCES);
+        mWorker.sendMessageDelayed(mWorker.obtainMessage(W.FREE_RESOURCES), FREE_DELAY);
+    }
+
+    protected void onStopSoundsW() {
+        int numStreamTypes = AudioSystem.getNumStreamTypes();
+        for (int i = numStreamTypes - 1; i >= 0; i--) {
+            ToneGenerator toneGen = mToneGenerators[i];
+            if (toneGen != null) {
+                toneGen.stopTone();
+            }
+        }
+    }
+
+    private ToneGenerator getOrCreateToneGeneratorW(int streamType) {
+        if (mToneGenerators[streamType] == null) {
+            try {
+                mToneGenerators[streamType] = new ToneGenerator(streamType,
+                        ToneGenerator.MAX_VOLUME);
+            } catch (RuntimeException e) {
+                if (false) {
+                    Log.d(TAG, "ToneGenerator constructor failed with "
+                            + "RuntimeException: " + e);
+                }
+            }
+        }
+        return mToneGenerators[streamType];
+    }
+
+    protected void onFreeResourcesW() {
+        synchronized (this) {
+            for (int i = mToneGenerators.length - 1; i >= 0; i--) {
+                if (mToneGenerators[i] != null) {
+                    mToneGenerators[i].release();
+                }
+                mToneGenerators[i] = null;
             }
         }
     }
