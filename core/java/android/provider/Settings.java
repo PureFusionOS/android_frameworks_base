e7bd752b35392772655193cfaf55a32ac59b60d5
Brett Rogers <brettrogers11@gmail.com>
2016-10-07 02:31:16 +0200

    Status bar notification ticker
    Change-Id: I29c2afc74376fa220ee88c7cec07d35d56c14680
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4206,6 +4206,11 @@ public final class Settings {
          */
 
         /**
+         * Whether to show the notification ticker on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_TICKER = "status_bar_show_ticker";
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
