07093297a1b218bb4352b0e0dcf14abb044564be
xiaomei.li <xiaomei.li@spreadtrum.com>
2016-11-23 13:55:32 +0800

    check whether the ActivityClientRecord of token is null when handleStopActivity
    This is to fix the nullpointer problem happen in the application process because
    of activity of the token is not found.
    
    Test: monkey test 10h with no reference problem happen.
    Change-Id: Ib69306f5938618b035905cf4dcb2f3d78df69008
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -3937,6 +3937,10 @@ public final class ActivityThread {
 
     private void handleStopActivity(IBinder token, boolean show, int configChanges, int seq) {
         ActivityClientRecord r = mActivities.get(token);
+        if (r == null) {
+            Log.w(TAG, "handleStopActivity: no activity for token:" + token);
+            return;
+        }
         if (!checkAndUpdateLifecycleSeq(seq, r, "stopActivity")) {
             return;
         }
