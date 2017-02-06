a8255ac49ae1af4a24c95e8d9619afc8f15ce5d2
beanstown106 <nbenis106@gmail.com>
2017-02-06 14:42:59 -0500

    incall vibration options [2/3]
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4129,6 +4129,24 @@ public final class Settings {
         public static final String TOAST_ICON = "toast_icon";
 
         /**
+         * Whether the phone vibrates on call connect
+         * @hide
+         */
+        public static final String VIBRATE_ON_CONNECT = "vibrate_on_connect";
+
+        /**
+         * Whether the phone vibrates on call waiting
+         * @hide
+         */
+        public static final String VIBRATE_ON_CALLWAITING = "vibrate_on_callwaiting";
+
+        /**
+         * Whether the phone vibrates on disconnect
+         * @hide
+         */
+        public static final String VIBRATE_ON_DISCONNECT = "vibrate_on_disconnect";
+
+        /**
          * Whether the phone volume up / down effect should be played
          * @hide
          */
