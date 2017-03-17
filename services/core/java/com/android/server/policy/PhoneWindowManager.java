c6b9d84e9545127a9491e5d4a1adbc76d2f5d77f
Alex Naidis <alex.naidis@linux.com>
2017-03-17 15:56:01 +0100

    Keypress Boost: Block multiple Keypress boosts
    We need to cover the case where a new Keypress boost
    gets scheduled while the previous Keypress boost is still
    running.
    
    As first step, this patch blocks new Keypress from coming
    up until the previous Keypress boost finished.
    
    Also we force a 50ms delay between the old Keypress boost
    and the new Keypress boost.
    
    Change-Id: If1192e282b2e0deffc90d2339587740f1bdde5ff
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -432,6 +432,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private boolean lIsPerfBoostEnabled;
     private int[] mBoostParamValWeak;
     private int[] mBoostParamValStrong;
+    private boolean mKeypressBoostBlocked;
 
     // FIXME This state is shared between the input reader and handler thread.
     // Technically it's broken and buggy but it has been like this for many years
@@ -807,17 +808,23 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int MSG_REQUEST_TRANSIENT_BARS_ARG_STATUS = 0;
     private static final int MSG_REQUEST_TRANSIENT_BARS_ARG_NAVIGATION = 1;
 
-    private CameraManager mCameraManager;
-    private boolean mTorchEnabled;
-    private boolean mIsTorchActive;
-    private boolean mWasTorchActive;
+    // AOSPA constants
+    private static final int MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK = 100;
 
+    boolean mWifiDisplayConnected = false;
+    int mWifiDisplayCustomRotation = -1;
+	
     private boolean mClearedBecauseOfForceShow;
     private boolean mTopWindowIsKeyguard;
     int mDesiredRotation = -1;
     private HardkeyActionHandler mKeyHandler;
 
     private boolean mHasPermanentMenuKey;
+	
+    private CameraManager mCameraManager;
+    private boolean mTorchEnabled;
+    private boolean mIsTorchActive;
+    private boolean mWasTorchActive;
 
     private class PolicyHandler extends Handler {
         @Override
@@ -909,6 +916,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 case HardkeyActionHandler.MSG_DO_HAPTIC_FB:
                     performHapticFeedbackLw(null,
                             HapticFeedbackConstants.LONG_PRESS, false);
+                    break;
+                case MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK:
+                    mKeypressBoostBlocked = false;
                     break;
             }
         }
@@ -4233,6 +4243,14 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (mBoostDuration != 0) {
             Slog.i(TAG, "Dispatching Keypress boost for " + mBoostDuration + " ms.");
             mPerf.perfLockAcquire(mBoostDuration, mBoostParamVal);
+
+            // Block Keypress boost
+            mKeypressBoostBlocked = true;
+
+            // Calculate unblock time and dispatch delayed unblock MSG
+            int mBoostBlockTime = mBoostDuration + 50/*ms*/;
+            mHandler.sendEmptyMessageDelayed(MSG_DISPATCH_KEYPRESS_BOOST_UNBLOCK, mBoostBlockTime);
+
         }
     }
 
@@ -6308,7 +6326,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
 
         // Intercept the Keypress event for Keypress boost
-        if (lIsPerfBoostEnabled) {
+        if (lIsPerfBoostEnabled && !mKeypressBoostBlocked) {
             dispatchKeypressBoost(keyCode);
         }
 
