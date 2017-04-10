c5488eeefc886069f187271631a9013ca3d25bad
xyyx <xyyx@mail.ru>
2017-04-10 16:15:18 +0800

    MTP_DIRTY_HACK: Fix USB Tethering
    Change-Id: I210133fe7f168c8950315695fd1cb6157452ff65
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4069,11 +4069,11 @@ public final class Settings {
          * @hide
          */
         public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
-
         /**
          * location of the clear all rectents button
          * @hide
          */
+
         public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
         /**
@@ -4372,6 +4372,8 @@ public final class Settings {
          * @hide
          */
         public static final String MTP_DIRTY_HACK = "mtp_dirty_hack";
+        public static final String MTP_DIRTY_HACK_SAVE = "mtp_dirty_hack_save";
+
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
