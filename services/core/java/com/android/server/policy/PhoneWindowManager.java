d6cb7b5241358479a51eafdddfc3ba3540e292c8
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-05-16 19:38:26 -0300

    Revert "Allow haptic feedback in navbar when hwkeys is off"
    This reverts commit 1199121ae6380d88c7d7f89f1a6adc49d9c9a836.
    
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6309,7 +6309,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
-                && event.getRepeatCount() == 0;
+                && event.getRepeatCount() == 0
+                && !isHwKeysDisabled();
 
         // Specific device key handling
         if (mDeviceKeyHandler != null) {
