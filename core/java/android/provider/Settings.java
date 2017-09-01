cbc09051056bbedfccf20c692bf847cea65a0d03
maxwen <max.weninger@gmail.com>
2017-09-01 15:54:53 +0200

    base: allow to swap volume buttons rotation based [1/2]
    Change-Id: Ia50d9d1d8a72a649dd86b99f9701b0de05252ebe
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4577,6 +4577,12 @@ public final class Settings {
         public static final String SWAP_NAVIGATION_KEYS = "swap_navigation_keys";
 
         /**
+         * Change volume up and down handlign based on rotation
+         * @hide
+         */
+        public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4751,6 +4757,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(AMBIENT_DOZE_CUSTOM_BRIGHTNESS);
             PRIVATE_SETTINGS.add(AMBIENT_DOZE_AUTO_BRIGHTNESS);
             PRIVATE_SETTINGS.add(SWAP_NAVIGATION_KEYS);
+            PRIVATE_SETTINGS.add(SWAP_VOLUME_BUTTONS);
         }
 
         /**
