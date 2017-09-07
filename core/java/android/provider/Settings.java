dbaa637779be618ce1398dd04cf5107362d7560a
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    [fwb] Ability to disable dashboards conditions (1/2)
    Change-Id: I653d50f76edad9af96555da5c71f01172fcd0de9
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3943,6 +3943,14 @@ public final class Settings {
         public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
         /**
+
+        /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
