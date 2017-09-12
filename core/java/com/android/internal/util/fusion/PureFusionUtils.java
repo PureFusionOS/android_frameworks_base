1f085b8e793fbf0bbcc47972224538179ef7f38c
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    [fwb] Add api to take screenshots abc: extracted from DUI add a public call to PFUtils
    Change-Id: Ica63362647eec89152d44a50ecdb83a2663b5c0c
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- core/java/com/android/internal/util/fusion/PureFusionUtils.java
+++ core/java/com/android/internal/util/fusion/PureFusionUtils.java
@@ -27,8 +27,11 @@ import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.fingerprint.FingerprintManager;
+import android.os.RemoteException;
 import android.net.ConnectivityManager;
 import android.net.ConnectivityManager;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 import android.net.NetworkInfo;
 import android.util.DisplayMetrics;
 import android.view.DisplayInfo;
@@ -39,6 +42,9 @@ import com.android.internal.R;
 import java.util.Locale;
 
 public class PureFusionUtils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     private static int sDeviceType = -1;
     private static final int DEVICE_PHONE = 0;
@@ -129,6 +135,15 @@ public class PureFusionUtils {
 
     public static boolean isTablet(Context context) {
         return getScreenType(context) == DEVICE_TABLET;
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     // Omni Switch Constants
