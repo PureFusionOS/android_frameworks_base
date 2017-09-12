5ce440b658863e347311593df0fa7affb983bf6d
bigrushdog <randall.rushing@gmail.com>
2017-09-12 14:14:35 +0800

    Add api to take screenshots * abc: extracted from DUI * add a public call to ActionUtils
    Change-Id: I01b0b72bf1a72063d6a7f97e6bdc8ad50b709ab9

--- core/java/com/android/internal/util/fusion/ActionUtils.java
+++ core/java/com/android/internal/util/fusion/ActionUtils.java
@@ -15,6 +15,8 @@ import android.os.UserHandle;
 import android.util.Log;
 
 import android.view.HapticFeedbackConstants;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 import android.widget.Toast;
 import com.android.internal.R;
 
@@ -24,6 +26,9 @@ public class ActionUtils {
     private static final boolean DEBUG = false;
     private static final String TAG = ActionUtils.class.getSimpleName();
     private static final String SYSTEMUI_PACKAGE = "com.android.systemui";
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     /**
      * Kills the top most / most recent user application, but leaves out the launcher.
@@ -157,5 +162,14 @@ public class ActionUtils {
         final PackageManager pm = context.getPackageManager();
         final ResolveInfo launcherInfo = pm.resolveActivityAsUser(launcherIntent, 0, userId);
         return launcherInfo.activityInfo.packageName;
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
