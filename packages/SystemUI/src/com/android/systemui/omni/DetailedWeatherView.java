62c90152c8bd375ef1d3a06bf988df016d6bede0
maxwen <max.weninger@gmail.com>
2017-10-18 00:38:11 +0200

    [frameworks/base] SystemUI: add missing text tint in weather details
    Change-Id: I8f2bf9e0925fb338479d8502ca88bb90623cf908
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
+++ packages/SystemUI/src/com/android/systemui/omni/DetailedWeatherView.java
@@ -263,7 +263,7 @@ public class DetailedWeatherView extends FrameLayout {
         final TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
         Typeface font = Typeface.create("sans-serif-condensed", Typeface.NORMAL);
         textPaint.setTypeface(font);
-        textPaint.setColor(Color.BLACK);
+        textPaint.setColor(getTintColor());
         textPaint.setTextAlign(Paint.Align.LEFT);
         final int textSize= (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 14f, resources.getDisplayMetrics());
         textPaint.setTextSize(textSize);
@@ -286,6 +286,19 @@ public class DetailedWeatherView extends FrameLayout {
         canvas.drawText(str, width / 2 - bounds.width() / 2, height - textSize / 2, textPaint);
 
         return new BitmapDrawable(resources, bmp);
+    }
+
+    private Drawable applyTint(Drawable icon) {
+        icon = icon.mutate();
+        icon.setTint(getTintColor());
+        return icon;
+    }
+
+    private int getTintColor() {
+        TypedArray array = mContext.obtainStyledAttributes(new int[]{android.R.attr.colorControlNormal});
+        int color = array.getColor(0, 0);
+        array.recycle();
+        return color;
     }
 
     private void forceRefreshWeatherSettings() {
