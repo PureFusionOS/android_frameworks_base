a7f4b9bc7b635e76c5c2f7c6a4489f1ea4e5d39f
Shubham Singh <coolsks94@gmail.com>
2017-11-12 02:19:26 +0530

    [fwb] Revert "Show 4G intead of LTE switch"
    This reverts commit c627d23a7de0e2d3e9d2801f18b15101507cc8b7.
    
    * Don't remove StatusBarSettingsObserver
    
    Change-Id: I253aa34955ac4a658ab73c96e1e01c6b39a1866b
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4120,13 +4120,6 @@ public final class Settings {
         /**
 
         /**
-         * Show four g instead of lte
-         * @hide
-         */
-        public static final String SHOW_FOURG = "show_fourg";
-
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
