3e754ce703cef3a63e7219b15b5598a8eb0faa9a
xyyx <xyyx@mail.ru>
2017-11-26 01:08:29 +0800

    frameworks/base: Don't report existing menu key if it was
    Change-Id: I092eef7ff4ad988da95c973828e88a346b7f7e55

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6296,6 +6296,11 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public boolean hasPermanentMenuKey() {
+        return mPolicy.hasPermanentMenuKey();
+    }
+
+    @Override
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
