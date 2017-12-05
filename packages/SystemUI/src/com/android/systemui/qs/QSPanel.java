9056db3b9d4bbbaa56e14c5e56c85a8154ee2fa6
maxwen <max.weninger@gmail.com>
2017-12-05 11:01:13 -0700

    [frameworks/base] SystemUI: add back QS hide label
    if enabled label click (secondary) will be reassigned to long click
    for dual target tiles
    
    Change-Id: I440362ab38057cbfba7750d75825179bd830c4e8
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>
    Signed-off-by: Tab Fitts <tfitts1212@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -413,11 +413,11 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
         r.callback = callback;
         r.tileView.init(r.tile);
         r.tile.refreshState();
-        r.tileView.setHideExpand(mTileLayout.getNumColumns() > 4);
         mRecords.add(r);
 
         if (mTileLayout != null) {
             mTileLayout.addTile(r);
+            configureTile(r.tile, r.tileView);
         }
 
         return r;
@@ -600,16 +600,37 @@ public class QSPanel extends LinearLayout implements Tunable, Callback {
         boolean updateResources();
         void updateSettings();
         int getNumColumns();
+        boolean isShowTitles();
 
         void setListening(boolean listening);
     }
 
+    private void configureTile(QSTile t, QSTileView v) {
+        if (mTileLayout != null) {
+            v.setHideExpand(mTileLayout.getNumColumns() > 4);
+            v.setHideLabel(!mTileLayout.isShowTitles());
+            if (t.isDualTarget()) {
+                if (!mTileLayout.isShowTitles()) {
+                    v.setOnLongClickListener(view -> {
+                        t.secondaryClick();
+                        return true;
+                    });
+                } else {
+                    v.setOnLongClickListener(view -> {
+                        t.longClick();
+                        return true;
+                    });
+                }
+            }
+        }
+    }
+
+    public void updateSettings() {
         if (mTileLayout != null) {
             mTileLayout.updateSettings();
 
             for (TileRecord r : mRecords) {
-                QSTileView v = r.tileView;
-                v.setHideExpand(mTileLayout.getNumColumns() > 4);
+                configureTile(r.tile, r.tileView);
             }
         }
     }
